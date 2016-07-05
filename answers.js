f8289479758ea12543e6f8fd98956e7e7b4ed324
Katarina <klingat@me.com>
2016-07-05 17:42:46 +0000

    added lastChar function

--- answers.js
+++ answers.js
@@ -6,4 +6,14 @@ function firstChar (string) {
 
 console.log(firstChar(""));
 console.log(firstChar("Hello!"));
-console.log(firstChar("82367"));
+console.log(firstChar("1994"));
+
+// Write a function that takes a string and returns the last character of a string. 
+
+function lastChar (string) {
+    return string[string.length - 1];
+}
+
+console.log(lastChar(""));
+console.log(lastChar("Hello!"));
+console.log(lastChar("1994"));
