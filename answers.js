f275313a08d9c6fddd177013f48ac397cb15fe8e
Katarina <klingat@me.com>
2016-07-05 17:38:26 +0000

    added firstChar function

---
+++ answers.js
@@ -0,0 +1,9 @@
+// Write a function that takes a string and returns the first character of the string.
+
+function firstChar (string) {
+    return string[0];
+}
+
+console.log(firstChar(""));
+console.log(firstChar("Hello!"));
+console.log(firstChar("82367"));
